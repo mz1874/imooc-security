2be532978e1b1e55ad3d861e4be4d17329c94c18
candy <mz1874@aliyun.com>
2020-07-20 00:39:30 +0800

    # 源码级别debug 用户登录

--- imooc-security-demo/src/main/java/com/candy/mz/controller/HelloWorldController.java
+++ imooc-security-demo/src/main/java/com/candy/mz/controller/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.candy.mz.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +23,28 @@ public class HelloWorldController {
         return "success";
     }
 
+    @GetMapping(value = "getMe")
+    public Object getMe() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
 
+    /**
+     * 获取 authentication
+     * @param authentication require false
+     * @return
+     */
+    @GetMapping(value = "getMe2")
+    public Object getMe2(Authentication authentication) {
+        return authentication;
+    }
+
+    /**
+     * 获取登录对象
+     * @param userDetails require false
+     * @return
+     */
+    @GetMapping(value = "getMe3")
+    public Object getMe3(@AuthenticationPrincipal UserDetails userDetails) {
+        return userDetails;
+    }
 }
